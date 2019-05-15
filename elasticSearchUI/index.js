ddf6a5e9f5ae4aab1aa3fe5b10dd071c39da2e47
axelkarl <axelkarl@kth.se>
2019-05-15 11:59:26 +0200

    Connected UI to data

--- elasticSearchUI/index.js
+++ elasticSearchUI/index.js
@@ -26,7 +26,6 @@ if (error) {
 }
 });
 
-
 // use the bodyparser as a middleware  
 app.use(bodyParser.json())
 // set port for the app to listen on
@@ -44,7 +43,7 @@ next();
 
 // defined the base route and return with an HTML file called tempate.html
 app.get('/', function(req, res){
-    res.sendFile('template.html', {
+    res.sendFile('index.html', {
        root: path.join( __dirname, 'views' )
      });
   })
@@ -53,18 +52,29 @@ app.get('/', function(req, res){
   app.get('/search', function (req, res){
     // declare the query object to search elastic search and return only 200 results from the first result found. 
     // also match any data where the name is like the query string sent in
+    console.log('inside app.get')
+    console.log(req.query)
     let body = {
       size: 200,
       from: 0, 
       query: {
-        match: {
-            name: req.query['q']
+
+        bool: {
+              must: [
+                {match: { declaration: req.query['query']}}
+              ]
         }
+        // match: {
+        //   // name: req.query['q']
+        //   declaration: req.query['q']
+        // }
+
       }
     }
     // perform the actual search passing in the index, the search query and the type
-    client.search({index:'scotch.io-tutorial',  body:body, type:'cities_list'})
+    client.search({index:req.query['type'],  body:body})
     .then(results => {
+      // console.log(results)
       res.send(results.hits.hits);
     })
     .catch(err=>{
