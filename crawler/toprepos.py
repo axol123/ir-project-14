60b938ce0c7516ecd7ac68208dc5a2ded5c3f713
axelkarl <axelkarl@kth.se>
2019-05-16 15:29:09 +0200

    show repo instead of line number

--- crawler/toprepos.py
+++ crawler/toprepos.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 
 res = urllib.request.urlopen(
-        "https://api.github.com/search/repositories?q=language:java&sort=stars&page=1&per_page=10&order=desc"
+        "https://api.github.com/search/repositories?q=language:java&sort=stars&page=1&per_page=3&order=desc"
 )
 res = json.loads(res.read().decode('utf-8'))
 
